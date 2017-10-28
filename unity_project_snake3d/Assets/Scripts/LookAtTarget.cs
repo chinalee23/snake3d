d4f51870c5a52d167ebd104e7b2b96d92c2dd7e3
aclisa <aclisa@yeah.net>
2017-10-28 14:19:18 +0800

    update
    0.camera上加入了grid图 方便对位置 一般来说蛇是永远位于屏幕中心点的.
    1.增加了point_01 作为蛇可以吃的分数-----需要新加一个蛇吃分数 身体变长的功能
    2.基于蛇身变长的逻辑 需要做camera沿local.rotation的轴(即垂直于屏幕的那根轴)拉远的功能
    3加入了lookattarget.cs   试试能否基于2.的逻辑使用look at的逻辑--因为目前的地形仅仅是一个简单的box空间.但是可能以后地形可能会复杂很多,建议用lookat,类似papa2 镜头聚焦人物的方式,在需要的区域做一定的偏移量.

---
+++ unity_project_snake3d/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Attaching this script to an object will make that object face the specified target.
+/// The most ideal use for this script is to attach it to the camera and make the camera look at its target.
+/// </summary>
+
+[AddComponentMenu("NGUI/Examples/Look At Target")]
+public class LookAtTarget : MonoBehaviour
+{
+	public int level = 0;
+	public Transform target;
+	public float speed = 8f;
+
+	Transform mTrans;
+
+	void Start ()
+	{
+		mTrans = transform;
+        if (target == null)
+        {
+            target = Camera.main.gameObject.transform;
+        }
+    }
+
+	void LateUpdate ()
+	{
+		if (enabled && target != null)
+		{
+            Vector3 rot = mTrans.eulerAngles;
+			Vector3 dir = target.position - mTrans.position;
+            //dir.y = 0f;
+            float mag = dir.magnitude;
+
+			if (mag > 0.001f)
+			{
+				Quaternion lookRot = Quaternion.LookRotation(dir);
+
+                rot.y = lookRot.eulerAngles.y + 90f;
+                rot.z = (lookRot.eulerAngles.x - 360f) * 0.2f + 90f;
+                lookRot = Quaternion.Euler(rot);
+				mTrans.rotation = Quaternion.Slerp(mTrans.rotation, lookRot, Mathf.Clamp01(speed * Time.deltaTime));
+			}
+		}
+	}
+}
