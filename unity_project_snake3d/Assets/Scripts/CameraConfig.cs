90d4184bacb95d1cf8771dbc3e7a2bacef820b59
chinalee23 <chinalee_lzh@126.com>
2017-11-20 23:36:59 +0800

    commit

--- unity_project_snake3d/Assets/Scripts/CameraConfig.cs
+++ unity_project_snake3d/Assets/Scripts/CameraConfig.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraConfig : MonoBehaviour {
+    public GameObject snake;
     public PlaneType pt;
 
     public void Save() {
-        Config.Save(pt, transform.position, transform.localEulerAngles);
+        if (snake != null) {
+            Config.Save(pt, transform.position - snake.transform.position, transform.localEulerAngles);
+        } else {
+            Config.Save(pt, transform.position, transform.localEulerAngles);
+        }
     }
 
     public void Clear() {
